bb11a6acfdb53e19e7891b1648f229da0504fe08
Mickael <jametmickael1@gmail.com>
2021-02-22 14:38:54 +0100

    création du model et des routes, début de categories\add.js

---
+++ app/routes/categories/add.js
@@ -0,0 +1,13 @@
+import Route from '@ember/routing/route';
+import { action } from '@ember/object';
+
+export default class CategoriesAddRoute extends Route {
+  model() {
+    return {};
+  }
+
+  @action save(data) {
+    let category = this.store.createRecord('category', data);
+    category.save().then(() => this.transitionTo('categories'));
+  }
+}
