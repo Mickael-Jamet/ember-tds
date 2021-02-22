18a8d19630f3d64514ed4c1b0def274b19e8993e
Mickael <jametmickael1@gmail.com>
2021-02-22 14:37:00 +0100

    création du model et des routes, début de categories\add.js

--- app/router.js
+++ app/router.js
@@ -13,4 +13,13 @@ Router.map(function () {
     this.route('add');
     this.route('update',{ path:'update/:contact_id' });
   });
+  this.route('categories', function() {
+    this.route('add');
+    this.route('update');
+    this.route('contacts', function() {
+      this.route('add');
+      this.route('update');
+    });
+  });
+  this.route('categories.add');
 });
